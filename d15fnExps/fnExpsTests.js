e71e08317fd7491ced8ca5d80651719a3c2fcca9
samueldesalegn <samueldesalegnt@gmail.com>
2023-03-04 19:49:09 -0600

    use strict may be

--- d15fnExps/fnExpsTests.js
+++ d15fnExps/fnExpsTests.js
@@ -1,4 +1,4 @@
-"use strict";
+// "use strict";
 
 /* You need the assert and function name declarations to test in node.  
 Comment these out when you send it to the browser with the index.html mocha setup page.
